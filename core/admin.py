8d1bcd5aa0a317c2e38de6a102b8d888e2dc2f55
Jair Torezone <jairtorezone@gmail.com>
2025-12-30 11:14:16 -0300

    Feat: Add Tabela Terceiro em admin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Categoria, Transacao, CartaoCredito, CompraCartao
+from .models import Categoria, Transacao, CartaoCredito, CompraCartao, Terceiro
 
 @admin.register(Transacao)
 class TransacaoAdmin(admin.ModelAdmin):
@@ -8,8 +8,13 @@ class TransacaoAdmin(admin.ModelAdmin):
 
 @admin.register(CompraCartao)
 class CompraCartaoAdmin(admin.ModelAdmin):
-    list_display = ('descricao', 'valor', 'data_compra', 'cartao')
+    list_display = ('descricao', 'valor', 'data_compra', 'cartao', 'terceiro')
     list_filter = ('cartao', 'data_compra')
+
+@admin.register(Terceiro)
+class TerceiroAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'relacionamento', 'usuario')
+    search_fields = ('nome',)
 
 # Registro simples
 admin.site.register(Categoria)
